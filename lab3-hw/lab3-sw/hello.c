38c211860bb72ef8cb270c73e190208dea566ffb
ignacio-rp <inaciorapo@gmail.com>
2020-05-09 07:07:09 -0400

    Software integration

---
+++ lab3-hw/lab3-sw/hello.c
@@ -0,0 +1,67 @@
+/*
+ * Userspace program that communicates with the vga_ball device driver
+ * through ioctls
+ *
+ * Stephen A. Edwards
+ * Columbia University
+ */
+
+#include <stdio.h>
+#include "vga_ball.h"
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+
+int vga_ball_fd;
+
+/* Read and print the coordinates */
+void print_coordinates() {
+  vga_ball_arg_t vla;
+  
+  if (ioctl(vga_ball_fd, VGA_BALL_READ_BACKGROUND, &vla)) {
+      perror("ioctl(VGA_BALL_READ_BACKGROUND) failed");
+      return;
+  }
+  printf("New_x: %d New_y: %d\n",
+	 vla.background.new_x, vla.background.new_y);
+}
+
+/* Set the new position of the sprite */
+void set_new_args(const vga_ball_color_t *c)
+{
+  vga_ball_arg_t vla;
+  vla.background = *c;
+  if (ioctl(vga_ball_fd, VGA_BALL_WRITE_BACKGROUND, &vla)) {
+      perror("ioctl(VGA_BALL_SET_BACKGROUND) failed");
+      return;
+  }
+}
+
+int main()
+{
+  vga_ball_arg_t vla;
+  int i;
+  static const char filename[] = "/dev/vga_ball";
+
+
+  printf("VGA ball Userspace program started\n");
+
+
+  if ( (vga_ball_fd = open(filename, O_RDWR)) == -1) {
+    fprintf(stderr, "could not open %s\n", filename);
+    return -1;
+  }
+
+  printf("initial state: ");
+  //print_coordinates();
+
+  vga_ball_color_t color = {1, 0, 16, 52};
+  set_new_args(&color);
+
+  printf("User Program Terminated\n");
+  return 0;
+}
